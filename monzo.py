ab1f294754128a221aa86d9f74394ac51761af4d
Scott Solkhon <scott.solkhon@sky.uk>
2016-11-15 20:09:37 +0000

    refactored to conform with pep8

--- monzo.py
+++ monzo.py
@@ -1,34 +1,39 @@
 #!/usr/bin/env python
 import requests
 
-def get_balance(acc_id, token):
-	'''
-	  Get user's balance
-	  returns float of current balance on user account
-	'''
-	params = {'account_id': acc_id}
-	url = 'https://api.monzo.com/balance'
-	response = requests.get(url=url, headers={'Authorization': 'Bearer ' + token}, params=params)
-	balance = float(response.json()['balance'] / 100)
 
-	return balance
+def get_balance(acc_id, token):
+    '''
+      Get user's balance
+      returns float of current balance on user account
+    '''
+    params = {'account_id': acc_id}
+    url = 'https://api.monzo.com/balance'
+    response = requests.get(url=url,
+                            headers={'Authorization': 'Bearer ' + token},
+                            params=params)
+    balance = float(response.json()['balance'] / 100)
+
+    return balance
+
 
 def get_transactions(acc_id, token, category=None):
-	'''
-	  Get transactions
-	  returns list of a users transactions
-	'''
-	params = {'account_id': acc_id}
-	url = 'https://api.monzo.com/transactions'
-	response = requests.get(url=url, headers={'Authorization': 'Bearer ' + token}, params=params)
-	transactions = response.json()['transactions']
+    '''
+      Get transactions
+      returns list of a users transactions
+    '''
+    params = {'account_id': acc_id}
+    url = 'https://api.monzo.com/transactions'
+    response = requests.get(url=url,
+                            headers={'Authorization': 'Bearer ' + token},
+                            params=params)
+    transactions = response.json()['transactions']
 
-	if category != None:
-		filter_transactions = []
-		for t in transactions:
-			if t['category'] == category:
-				filter_transactions.append(t)
-		return filter_transactions		
-	
-	return transactions
+    if category is not None:
+        filter_transactions = []
+        for t in transactions:
+            if t['category'] == category:
+                filter_transactions.append(t)
+        return filter_transactions
 
+    return transactions
